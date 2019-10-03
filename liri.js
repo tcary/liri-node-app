7982bd01b900c8fde9e064abe8f81f489a6a0c15
Tetiana Cary <taniushka.tata@gmail.com>
2019-10-03 09:13:47 -0600

    ;Updated functions for getSong and getMovie. Added a function for doSays.

--- liri.js
+++ liri.js
@@ -8,28 +8,30 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
-var query = process.argv.slice(3).join("");
+var query = process.argv.slice(3).join(" ");
 var text = "random.txt";
 
 
 // console.log(query);
+function doSome() {
 
-switch (command) {
-    case "concert-this":
-        getBands(query);
-        break;
-    case "spotify-this-song":
-        getSong(query);
-        break;
-    case "movie-this":
-        getMovie(query);
-        break;
-    case "do-what-it-says":
-        doSays();
-        break;
-    default:
-        console.log("Invalid Option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
-        break;
+    switch (command) {
+        case "concert-this":
+            getBands(query);
+            break;
+        case "spotify-this-song":
+            getSong(query);
+            break;
+        case "movie-this":
+            getMovie(query);
+            break;
+        case "do-what-it-says":
+            doSays();
+            break;
+        default:
+            console.log("Invalid Option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
+            break;
+    }
 }
 function getBands(query) {
 
@@ -42,7 +44,7 @@ function getBands(query) {
             for (var i = 0; i < response.data.length; i++) {
                 var datetime = response.data[i].datetime; //Saves datetime response into a variable
                 dateFormat = ("");
-                var dateArr = moment(datetime.dateFormat); //Attempting to split the date and time in the response
+                var dateArr = moment(datetime.dateFormat); // split the date and time in the response
                 // console.log(dateArr);
 
                 var concertResults =
@@ -58,43 +60,61 @@ function getBands(query) {
         })
 }
 
-// function getSong(query) {
-//     if (query === undefined) {
-//         query = "The Sign"; // default song
-//     }
+function getSong(query) {
+    if (!query) {
+        query = "The Sign by Ace of Base"; // default song
+    } console.log(query);
+    spotify.search({ type: 'track', query: query })
+        .then(function (response) {
 
-// }
+            console.log(response.tracks.items[0]);
+            var items = response.tracks.items[0];
+            console.log(items.album.artists[0].name);
+            console.log(items.name);
+            console.log(items.album.name);
+            console.log(items.preview_url);
+
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
 function getMovie(query) {
     axios({
         method: "get",
         url: "http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy",
     })
         .then(function (response) {
-            // console.log(response)
-            for (var i = 0; i < response.data.length; i++) {
+            //console.log(response.data)
 
-                var movieResults =
-                    "--------------------------------------------------------------------" +
-                    "\nMovie Title: " + response.data[i].title +
-                    "\nRelease Year: " + response.data[i].year +
-                    "\nIMDB Rating: " + response.data[i].imdbRating +
-                    "\nRotten Tomatoes Rating: " + response.data[i].imdbRating +
-                    "\nCountry Production: " + response.data[i].country +
-                    "\nLanguage: " + response.data[i].language +
-                    "\nMovie Plot: " + response.data[i].plot +
-                    "\nActors: " + response.data[i].actors
-                console.log(movieResults);
-            }
+            var movieResults =
+                "--------------------------------------------------------------------" +
+                "\nMovie Title: " + response.data.Title +
+                "\nRelease Year: " + response.data.Year +
+                "\nIMDB Rating: " + response.data.imdbRating +
+                "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                "\nCountry Production: " + response.data.Country +
+                "\nLanguage: " + response.data.Language +
+                "\nMovie Plot: " + response.data.Plot +
+                "\nActors: " + response.data.Actors
+            console.log(movieResults);
         })
         .catch(function (err) {
             console.log(err);
         })
 }
-// function doSays(query) {
-//     fs.readFile(text, "utf8", function (err, data) {
-//         if (err) {
-//             return console.log(err);
-//         }
+function doSays() {
+    fs.readFile(text, "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        } else {
+            // console.log(data);
+            var randomArr = data.split(",");
+            command = randomArr[0];
+            query = randomArr[1];
+            doSome();
+        }
+    })
+}
 
-//     })
-// }
+doSome();
